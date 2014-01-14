ced740dab5f94557e02e55cf530b581517da5361
Carl Haken <carlhaken@gmail.com>
2014-01-13 22:28:54 -0500

    arduino async method almost done.

--- arduinoSerial.py
+++ arduinoSerial.py
@@ -7,9 +7,6 @@ Write to stdin to serial
 import serial
 import threading
 from multiprocessing import Process, Pipe
-import sys
-import re
-import time
 
 class SyncArduino:
     def __init__(self, port='COM7', baud=115200, timeout=1):
@@ -26,17 +23,18 @@ def get_sync_arduino(port, baud, timeout):
 def child_process_event_loop(conn, fps, slices):
     while conn.poll(3):
         try:
-            x, y = conn.recv()
-            move_mouse(x,y)
+            x, y, port, baud = conn.recv()
+            move_mouse_interp(x,y, fps, slices, port, baud)
         except:
             continue
 
-def move_mouse(x, y):
+def move_mouse(x, y, port, baud):
     #print "move %s,%s\n\n" % (x, y)
     h = open('./templog', 'a')
-    h.write("move %s,%s\n" % (str(x), str(y)))
+    serial_string = "32100\n%d\n%d\n" % (int(x),int(y))
+    h.write(serial_string)
 
-def move_mouse_interp(x, y, fps, interp_slices):
+def move_mouse_interp(x, y, fps, interp_slices, port, baud):
     #Split commands into the a series of commands and schedule them to run
     interp_data = []
 
@@ -55,7 +53,7 @@ def move_mouse_interp(x, y, fps, interp_slices):
     i = 0
     inc = 1.0 / (fps * interp_slices)
     for x, y in interp_data:
-        threading.Timer(inc * i, move_mouse, [x,y]).start()
+        threading.Timer(inc * i, move_mouse, [x,y, port, baud]).start()
         i += 1
 
 
@@ -64,6 +62,8 @@ class AsyncArduino:
         #self.connection = serial.Serial(port, baud, timeout=timeout)
         self.fps = fps
         self.interp_slices = slices
+        self.port = port
+        self.baud = baud
 
         parent_conn, child_conn = Pipe()
         p = Process(target=child_process_event_loop, args=(child_conn, fps, slices))
@@ -76,24 +76,16 @@ class AsyncArduino:
         self.__child_process.terminate()
         pass
 
-    def send_to_child(self, xy_delta):
-        self.__child_process_connection.send(xy_delta)
+    def mouse_move(self, x, y):
+        self.__child_process_connection.send((x, y, self.port, self.baud))
 
 
-def get_async_arduino(port, baud, timeout):
-    return AsyncArduino(port, baud, timeout)
-
-'''            data = re.split('\,', message)
-
-            control_code = int(message_data.pop(0))
-
-            if control_code == 32100 and interp_slices > 1:
-                interp_data = []
-
-'''
+def get_async_arduino(port, baud, timeout, fps=30, slices=4):
+    return AsyncArduino(port, baud, timeout, fps, slices)
 
 if __name__ == '__main__':
-    ard = get_async_arduino(5, 9600, 1)
+    ard = get_async_arduino(5, 9600, 1, slices=10, fps=10)
     #ard.move_mouse_interp(100,90)
-    ard.send_to_child((50,60))
+
+    ard.mouse_move(50,60)
 
