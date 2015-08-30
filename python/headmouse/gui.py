27df888c617485174c86c0ffba84c4247c560c47
Carl Haken <carlhaken@gmail.com>
2015-08-30 13:04:26 -0700

    Working config file manipulation

--- python/headmouse/gui.py
+++ python/headmouse/gui.py
@@ -1,51 +1,57 @@
 #!/usr/bin/env python
 
 from Tkinter import *
+import conf
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 root=Tk()
-
+conf.initialize()
+pp.pprint(conf.current_config)
 
 def stopProg(e):
     root.destroy()
 
-def callback(test):
-    print "hi"
-    print dir(test.widget)
-    print (test.widget.get())
+def set_conf_parameter(name, value):
+    conf.current_config[name] = value
+    conf.apply_changes()
 
-def add_slider(master):
-    w = Scale(master, from_=0, to=100, resolution=0.1, label="test")
-    w.bind("<ButtonRelease-1>", callback)
+def add_slider(root, name=None, scale_data=None, initial=None):
+    w = Scale(root, from_=scale_data[0], to=scale_data[1], resolution=scale_data[2], label=name)
+    w.set(initial)
+    w.bind("<ButtonRelease-1>", lambda event: set_conf_parameter(name, event.widget.get()))
     w.pack(padx=5, pady=10, side=LEFT)
 
+def config_root(root):
+    button1=Button(root,
+          text="Quit")
 
-button1=Button(root,
-      text="Hello World click to close")
+    button1.pack()
+    button1.bind('<Button-1>',stopProg)
 
-button1.pack()
-button1.bind('<Button-1>',stopProg)
+    w = 800 # width for the Tk root
+    h = 650 # height for the Tk root
 
-w = 800 # width for the Tk root
-h = 650 # height for the Tk root
-
-# get screen width and height
-ws = root.winfo_screenwidth() # width of the screen
-hs = root.winfo_screenheight() # height of the screen
+    # get screen width and height
+    ws = root.winfo_screenwidth() # width of the screen
+    hs = root.winfo_screenheight() # height of the screen
 
 
-x = (ws/2) - (w/2)
-y = (hs/2) - (h/2)
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
 
-# set the dimensions of the screen
-# and where it is placed
-root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    # set the dimensions of the screen
+    # and where it is placed
+    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-root.title("app")
+    root.title("Headmouse")
+    root.lift ()
 
-add_slider(root)
-add_slider(root)
+config_root(root)
 
-root.lift ()
+for name, scale in conf.scale_data.iteritems():
+    print "name:%s, scale:%s" % (name, scale)
+    add_slider(root, **{'name':name, 'scale_data':scale, 'initial':0})
 
 root.mainloop()
 
