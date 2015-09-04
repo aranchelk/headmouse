8e4d88ba2139b13571a3137df05afa9b8c191bda
Carl Haken <carlhaken@gmail.com>
2015-09-03 22:15:45 -0700

    Laggy mouse pedal working.

--- arduinoHm/arduinoHm.ino
+++ arduinoHm/arduinoHm.ino
@@ -1,6 +1,9 @@
 int serValue = 0;
 int serData[10];
 int controlM = 0;
+// Button values are reversed because of pullup resistors
+int pedalUp = 1;
+int pedalDown = 0;
 
 void setup() {  
   //Setup mouse buttons
@@ -45,34 +48,55 @@ void loop() {
       yCarryOver -= y;
     }
     
+    //Temporary pedal logic
+    if(readLeftPedal() == pedalDown){
+      while (readLeftPedal() == pedalDown){
+        delay(10);
+      }
+      
+      Mouse.click();   
+    }
+    
     
     if (Serial1.find("c")) {
       // We've received the start of a control code
       ctrlCode = Serial1.parseInt();
       
-      if (ctrlCode == 0) {
-        // Request for device info
-        Serial1.println("hm0.0.1");
-      }
-        
-      if (ctrlCode == 1) {
-        // Received code to move mouse
-        
-        xCarryOver += Serial1.parseInt();
-        yCarryOver += Serial1.parseInt();
-      }
-      
-      if (ctrlCode == 2) {
-        maxMove = Serial1.parseInt();
-      }
-      
-      if (ctrlCode == 3) {
-        Serial1.println("echo");
-      }
-      
-    }
-              
+      switch (ctrlCode) {
+        case 0:
+          // Request for device info
+          Serial1.println("hm0.0.1");
+          break;
+        case 1:
+          // Received code to move mouse
+          xCarryOver += Serial1.parseInt();
+          yCarryOver += Serial1.parseInt();
+          break;
+        case 2:
+          maxMove = Serial1.parseInt();
+          break;
+        case 3:
+          //Serial1.println("echo");
+          //print out the value of the pushbutton
+          Serial1.println();
+          break;
+        default: 
+          // if nothing else matches, do the default
+          // default is optional
+        break;
+      } 
+    } else {
+        //Serial1.println("No command received.");
+    }        
   }
+}
+
+int readLeftPedal() {
+  return digitalRead(2);
+}
+
+int readRightPedal() {
+  return digitalRead(3);
 }
 
 int absMax(int val, int maxVal) {
