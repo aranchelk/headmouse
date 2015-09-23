a4c14f3154fc81e9fd2bd04957c256499817d824
Carl <carlhaken@gmail.com>
2015-09-22 22:38:02 -0700

    Implement reset.

--- python/headmouse/hm2.py
+++ python/headmouse/hm2.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 import sys
+import os
 import gui_menu
 from multiprocessing import Process, Pipe
 import util
@@ -12,11 +13,20 @@ from cameras import v4l2_loopback_camera as camera
 def f(conn):
     gui_menu.initialize(conn)
 
+def restart():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
 
 if __name__ == '__main__':
     parent_conn, child_conn = Pipe()
     p = Process(target=f, args=(child_conn,))
     p.start()
+
+    def restart():
+        p.terminate()htn
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
 
     fps = util.simple_fps()
     f = 180.0
@@ -31,15 +41,26 @@ if __name__ == '__main__':
     #     'gray_scale':False
     # }
 
-    camera_config = parent_conn.recv()
+    camera_config = parent_conn.recv()['config']
 
-    with Vision(camera.Camera(camera_config), camera_config) as cam:
+    with camera.Camera(**camera_config) as cam:
         display_frame = util.Every_n(3, cam.display_image)
 
         while True:
             try:
                 if parent_conn.poll(.001):
-                    camera_config.update(parent_conn.recv())
+                    pipe_data = parent_conn.recv()
+
+                    if 'config' in pipe_data:
+                        camera_config.update(pipe_data['config'])
+                        print(camera_config)
+                    elif 'control' in pipe_data:
+                        control_message = pipe_data['control']
+
+                        if control_message == 'restart':
+                            restart()
+                        else:
+                            print(control_message)
                 else:
                     if not p.is_alive():
                         sys.exit("GUI component has terminated.")
