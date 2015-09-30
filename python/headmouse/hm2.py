24ab24d6f334ddc8375004d432be8d9a817581af
Carl <carlhaken@gmail.com>
2015-09-29 21:11:52 -0700

    Add realtime updates to camera gain and brightness.

--- python/headmouse/hm2.py
+++ python/headmouse/hm2.py
@@ -6,8 +6,11 @@ import os
 import gui_menu
 from multiprocessing import Process, Pipe
 import util
+import conf
 from naive_dots_vision import Vision
 from cameras import v4l2_loopback_camera as camera
+
+current_config = conf.render()
 
 
 def f(conn):
@@ -41,42 +44,47 @@ if __name__ == '__main__':
     #     'gray_scale':False
     # }
 
-    camera_config = parent_conn.recv()['config']
+    conf_from_gui = parent_conn.recv()['config']
+    print(conf_from_gui)
+    current_config.update_all(conf_from_gui)
+    current_config.register_callback('smoothing', lambda x, y: print("lambda print:", x, y))
+    print(current_config.callbacks)
 
-    with camera.Camera(**camera_config) as cam:
-        display_frame = util.Every_n(3, cam.display_image)
+    with camera.Camera(current_config) as cam:
+        with Vision(cam, current_config) as viz:
+            display_frame = util.Every_n(3, viz.display_image)
 
-        while True:
-            try:
-                if parent_conn.poll(.001):
-                    pipe_data = parent_conn.recv()
+            while True:
+                try:
+                    if parent_conn.poll(.001):
+                        pipe_data = parent_conn.recv()
 
-                    if 'config' in pipe_data:
-                        camera_config.update(pipe_data['config'])
-                        print(camera_config)
-                    elif 'control' in pipe_data:
-                        control_message = pipe_data['control']
+                        if 'config' in pipe_data:
+                            current_config.update_all(pipe_data['config'])
+                            # print(conf.current_config)
+                        elif 'control' in pipe_data:
+                            control_message = pipe_data['control']
 
-                        if control_message == 'restart':
-                            print('restart')
-                            restart()
-                        else:
-                            print(control_message)
-                else:
-                    if not p.is_alive():
-                        sys.exit("GUI component has terminated.")
+                            if control_message == 'restart':
+                                print('restart')
+                                restart()
+                            else:
+                                print(control_message)
+                    else:
+                        if not p.is_alive():
+                            sys.exit("GUI component has terminated.")
 
-                cam.get_image()
-                cam.process()
+                    viz.get_image()
+                    viz.process()
 
-                display_frame.next()
-                print_fps.next()
+                    display_frame.next()
+                    print_fps.next()
 
 
-            except KeyboardInterrupt:
-                p.terminate()
-                #p.join()
-                sys.exit()
+                except KeyboardInterrupt:
+                    p.terminate()
+                    #p.join()
+                    sys.exit()
 
 
 
