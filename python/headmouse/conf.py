ba5297b10083e1e1f73b641ad0541de23b0fb7b0
Carl <carlhaken@gmail.com>
2015-09-29 20:13:24 -0700

    Refactor conf to be an observable dict with callbacks.

--- python/headmouse/conf.py
+++ python/headmouse/conf.py
@@ -6,9 +6,34 @@ import ConfigParser
 USER_CONFIG_FILE = os.path.expanduser("~/.headmouse")
 TEMP_FILE = os.path.expanduser("~/.headmouse.scratch")
 config_parser = ConfigParser.SafeConfigParser()
-current_config = None
 
-base_config = {
+
+class ObservableDict(dict):
+    def __init__(self, *args, **kwargs):
+        self.callbacks = {}
+        super(ObservableDict, self).__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        # We don't set the same key to the value twice to avoid triggering callbacks unnecessarily
+        if self[key] != value:
+            if key in self.callbacks:
+                for f in self.callbacks[key]:
+                    f(key, value)
+                    # was debugging multiple instances of module print(id(self))
+        super(ObservableDict, self).__setitem__(key, value)
+
+    def register_callback(self, conf_key, function):
+        if conf_key in self.callbacks:
+            self.callbacks[conf_key].append(function)
+        else:
+            self.callbacks[conf_key] = [function]
+
+    def update_all(self, new_val_dict):
+        for k, v in new_val_dict.iteritems():
+            self[k] = new_val_dict[k]
+
+# Contains default values and establishes types for values parsed from ini files
+template_config = {
         'output': 'arduino_output',
         'arduino_baud': 115200,
         # 'arduino_port': '/dev/tty.usbmodemfa13131',
@@ -43,6 +68,8 @@ base_config = {
 
         'verbosity': 0,
         }
+
+# Template config Metadata, currently used for settings GUI rendering, could also be used for validation.
 option_menu_data = {
     'algorithm': ['naive_dots_vision', 'eye_haar_vision'],
     'camera': ['v4l2_looback_camera', 'simple_camera']
@@ -58,46 +85,45 @@ scale_data = {
 }
 
 
-def config_from_file(config_file):
+def from_file(file_path, template):
     try:
-        config_parser.read([config_file])
-        return dict(config_parser.items('headmouse'))
-    except:
-        # Todo: don't except any exception
+        config_parser.read([file_path])
+        conf = dict(config_parser.items('headmouse'))
+
+        if template is not None:
+            # Cast values from config parser to match default as parser always returns dict of strings
+            for k,v in template.iteritems():
+                conf[k] = type(v)(conf[k])
+        return conf
+
+    except ConfigParser.NoSectionError:
+        print("No config available!")
         config_parser.add_section('headmouse')
         return {}
 
 
 def render_config():
-    # Applies default base config to user_config_file
-    full_conf = base_config.copy()
-    full_conf.update(config_from_file(USER_CONFIG_FILE))
-
-    # Cast values from config parser to match default as parser always returns dict of strings
-    for k,v in base_config.iteritems():
-        full_conf[k] = type(v)(full_conf[k])
-
-    return full_conf
+    # Apply default base config to user_config_file
+    full_conf = template_config.copy()
+    full_conf.update(from_file(USER_CONFIG_FILE, full_conf))
+    return ObservableDict(full_conf)
 
 
-def write_to_file(file_name):
-    for k, v in current_config.iteritems():
+def save(conf, path=USER_CONFIG_FILE):
+    for k, v in conf.iteritems():
         config_parser.set('headmouse', k, str(v))
 
-    with open(file_name, 'w+') as f:
+    with open(path, 'w+') as f:
         config_parser.write(f)
 
 
-def save_changes():
-    write_to_file(USER_CONFIG_FILE)
-
-
-def initialize():
-    global current_config
-    current_config = render_config()
-
-
 if __name__ == "__main__":
-    initialize()
-    print(current_config)
+    c = render_config()
+    print(c)
+    c.register_callback('smoothing', lambda x, y: print(x, y))
+    c['smoothing'] = .2
+    c['smoothing'] = .2
+
+    #save(c)
+
 
